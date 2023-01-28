d96d1d9af9d8e2752f7323801f341d520eee6eb6
Peeranut-Kit <peeranut.kit.work@gmail.com>
2023-01-28 18:10:22 +0700

    Add server.js and hospitals router

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const dotenv = require('dotenv');
+//Route files
+const hospitals = require('./routes/hospitals');
+
+//Load env vars
+dotenv.config({path:'./config/config.env'});
+
+const app = express();
+
+/*app.get('/', (req,res) => {
+    // res.send('<h1>Hello from express</h1>');
+    // res.send({name:'Brad'});
+    // res.json({name:'Brad'});
+    // res.sendStatus(400);
+    // res.status(400).json({success:false});
+    res.status(200).json({status:true, data:{id:1}});
+});*/
+
+//Mount routers
+app.use('./api/v1/hospitals', hospitals);
+
+const PORT = process.env.PORT || 5001;
+
+app.listen(PORT, console.log('Server running in ', process.env.NODE_ENV, ' mode on port ', PORT));
