0ad7c904dd8971c9afdcb6777728a5f4e17ad675
Peeranut "Max" Kit <peeranut.kit.work@gmail.com>
2023-03-16 21:04:40 +0700

    Cascading Delete

--- models/Hospital.js
+++ models/Hospital.js
@@ -34,6 +34,13 @@ const HospitalSchema = new mongoose.Schema({
     toObject: {virtuals: true}
 });
 
+//Cascade delete appointments when a hospital is deleted
+HospitalSchema.pre('remove', async function(next) {
+    console.log(`Appointments being removed from hospital ${this._id}`);
+    await this.model('Appointment').deleteMany( { hospital: this._id} );
+    next();
+})
+
 //Reverse populate with virtuals
 HospitalSchema.virtual('appointments', {
     ref: 'Appointment',
