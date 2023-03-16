955627fe17f6ae5ef67b1814d6b6f5d392a9c88a
Peeranut "Max" Kit <peeranut.kit.work@gmail.com>
2023-03-16 22:35:17 +0700

    Get, Add, Update & Delete Appointment

--- controllers/appointments.js
+++ controllers/appointments.js
@@ -1,4 +1,6 @@
 const Appointment = require('../models/Appointment');
+const Hospital = require('../models/Hospital');
+const { login } = require('./auth');
 
 //@desc     Get all appointments
 //@route    GET /api/v1/appointments
@@ -32,3 +34,89 @@ exports.getAppointments= async (req,res,next)=>{
         return res.status(500).json({success:false, message: "Cannot find Appointment"}); 
     }
 };
+
+//@desc     Get single appointment
+//@route    GET /api/v1/appointments/:id
+//@access   Public
+exports.getAppointment= async (req,res,next)=>{
+    try {
+        const appointment = await Appointment.findById(req.params.id).populate({
+            path: 'hospital',
+            select: 'name description tel'
+        });
+        if(!appointment){
+            return res.status(400).json({success:false, message:`No appointment with the id of ${req.params.id}`});
+        }
+        res.status(200).json({success:true, data:appointment});
+    } catch (error) {
+        console.log(error);
+        res.status(400).json({success:false, message:"Cannot find Appointment"});
+    }
+};
+
+//@desc     Add appointment
+//@route    POST /api/v1/hospitals/:hospitalId/appointment
+//@access   Private
+exports.addAppointment= async (req,res,next)=>{
+    try {
+        req.body.hospital = req.params.hospitalId;
+        const hospital = await Hospital.findById(req.params.hospitalId);
+
+        if (!hospital) {
+            return res.status(404).json({ success: false, message: `No hospital with the id of ${req.params.hospitalId}`});
+        }
+        const appointment = await Appointment.create(req.body);
+        res.status(200).json({
+            success: true,
+            data: appointment
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ success: false, message: 'Cannot create Appointment'});
+    }
+};
+
+//@desc     Update appointment
+//@route    PUT /api/v1/appointments/:id
+//@access   Private
+exports.updateAppointment= async (req,res,next)=>{
+    try {
+        let appointment = await Appointment.findById(req.params.id);
+        if(!appointment){
+            return res.status(404).json({ success: false, message: `No appointment with the id of ${req.params.id}`});
+        }
+
+        appointment = await Appointment.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+            runValidators: true
+        });
+        res.status(200).json({
+            success: true,
+            data: appointment
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ success: false, message: 'Cannot update Appointment'});
+    }
+}
+
+//@desc     Delete appointment
+//@route    DELETE /api/v1/appointments/:id
+//@access   Private
+exports.deleteAppointment= async (req,res,next)=>{
+    try {
+        const appointment = await Appointment.findById(req.params.id);
+        if(!appointment){
+            return res.status(404).json({ success: false, message: `No appointment with the id of ${req.params.id}`});
+        }
+
+        await Appointment.remove();
+        res.status(200).json({
+            success: true,
+            data: {}
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ success: false, message: 'Cannot delete Appointment'});
+    }
+}
--- routes/appointments.js
+++ routes/appointments.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router({mergeParams: true});
 
-const {getAppointments} = require('../controllers/appointments');
+const {getAppointments, getAppointment, addAppointment, updateAppointment, deleteAppointment} = require('../controllers/appointments');
 const {protect} = require('../middleware/auth');
 
-router.route('/').get(protect, getAppointments);
+router.route('/').get(protect, getAppointments).post(addAppointment);
+router.route('/:id').get(getAppointment).put(updateAppointment).delete(deleteAppointment);
 
 module.exports = router;
