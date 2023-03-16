39d289b2029d304531e9d5a707158fffc3bdcd6c
Peeranut "Max" Kit <peeranut.kit.work@gmail.com>
2023-03-16 23:14:54 +0700

    Appointment Ownership & Protection

--- controllers/appointments.js
+++ controllers/appointments.js
@@ -47,7 +47,10 @@ exports.getAppointment= async (req,res,next)=>{
         if(!appointment){
             return res.status(400).json({success:false, message:`No appointment with the id of ${req.params.id}`});
         }
-        res.status(200).json({success:true, data:appointment});
+        res.status(200).json({
+            success:true,
+            data:appointment
+        });
     } catch (error) {
         console.log(error);
         res.status(400).json({success:false, message:"Cannot find Appointment"});
@@ -65,7 +68,20 @@ exports.addAppointment= async (req,res,next)=>{
         if (!hospital) {
             return res.status(404).json({ success: false, message: `No hospital with the id of ${req.params.hospitalId}`});
         }
-        const appointment = await Appointment.create(req.body);
+
+        //Add user Id to req.body
+        req.body.user = req.user.id;
+        //Check for existed appointment
+        const existedAppointments = await Appointment.find( {user: req.user.id} );
+        //If the user is not an admin, they can only create 3 appointments.
+        if(existedAppointments.length >= 3 && req.user.role !== 'admin') {
+            return res.status(400).json({
+                success: false,
+                message: `The user with ID ${req.user.id} has already made 3 appointments`
+            });
+        }
+
+         const appointment = await Appointment.create(req.body);
         res.status(200).json({
             success: true,
             data: appointment
@@ -84,6 +100,14 @@ exports.updateAppointment= async (req,res,next)=>{
         let appointment = await Appointment.findById(req.params.id);
         if(!appointment){
             return res.status(404).json({ success: false, message: `No appointment with the id of ${req.params.id}`});
+        }
+
+        //Make sure user is the appointment owner
+        if(appointment.user.toString() !== req.user.id && req.user.role !== 'admin') {
+            return res.status(401).json({
+                success: false,
+                message: `User ${req.user.id} is not authorized to update this appointment`
+            });
         }
 
         appointment = await Appointment.findByIdAndUpdate(req.params.id, req.body, {
@@ -110,6 +134,14 @@ exports.deleteAppointment= async (req,res,next)=>{
             return res.status(404).json({ success: false, message: `No appointment with the id of ${req.params.id}`});
         }
 
+        //Make sure user is the appointment owner
+        if(appointment.user.toString() !== req.user.id && req.user.role !== 'admin') {
+            return res.status(401).json({
+                success: false,
+                message: `User ${req.user.id} is not authorized to delete this appointment`
+            });
+        }
+        
         await Appointment.remove();
         res.status(200).json({
             success: true,
