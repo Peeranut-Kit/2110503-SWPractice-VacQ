59d51f32dcf0a73cb8b001069b3d15b91f69c1a7
Peeranut "Max" Kit <peeranut.kit.work@gmail.com>
2023-03-16 22:39:03 +0700

    Protect routes

--- routes/appointments.js
+++ routes/appointments.js
@@ -2,9 +2,14 @@ const express = require('express');
 const router = express.Router({mergeParams: true});
 
 const {getAppointments, getAppointment, addAppointment, updateAppointment, deleteAppointment} = require('../controllers/appointments');
-const {protect} = require('../middleware/auth');
+const {protect, authorize} = require('../middleware/auth');
 
-router.route('/').get(protect, getAppointments).post(addAppointment);
-router.route('/:id').get(getAppointment).put(updateAppointment).delete(deleteAppointment);
+router.route('/')
+    .get(protect, getAppointments)
+    .post(protect, authorize('admin','user'), addAppointment);
+router.route('/:id')
+    .get(protect, getAppointment)
+    .put(protect, authorize('admin', 'user'), updateAppointment)
+    .delete(protect, authorize('admin', 'user'), deleteAppointment);
 
 module.exports = router;
